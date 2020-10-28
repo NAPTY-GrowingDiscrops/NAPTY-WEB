a639b02aa73038998f1751667a97d4403d2b7edd
jungbin0722 <jungbin4337@dgsw.hs.kr>
2020-10-28 09:02:36 +0900

    [FIX] router

--- GDproject_Server/src/router/auth/index.js
+++ GDproject_Server/src/router/auth/index.js
@@ -4,8 +4,8 @@ const authCtrl = require('./auth.Ctrl');
 router.post('/login', authCtrl.login);
 router.post('/register', authCtrl.register);
 
-router.post('/mailCheck', authCtrl.mailCheck);
-router.post('/nameCheck', authCtrl.nameCheck);
-router.post('/idCheck', authCtrl.idCheck);
+router.post('/register/mailCheck', authCtrl.mailCheck);
+router.post('/register/nameCheck', authCtrl.nameCheck);
+router.post('/register/idCheck', authCtrl.idCheck);
 
 module.exports = router;
