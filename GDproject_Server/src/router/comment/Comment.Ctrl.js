9655f50656a736e1b6d5c9ad4dea877fa061b649
jungbin0722 <jungbin4337@dgsw.hs.kr>
2020-11-02 09:28:54 +0900

    [MAKE] comment

--- GDproject_Server/src/router/comment/Comment.Ctrl.js
+++ GDproject_Server/src/router/comment/Comment.Ctrl.js
@@ -2,10 +2,25 @@ const models = require('../../../models');
 
 exports.createComment = async (req, res) => {
     const { body, user } = req;
+    const { idx } = req.params.idx;
+
+    if (!user) {
+        return res.status(401).jsoe({
+            message: "로그인을 먼저 해주세요!",
+        });
+    }
 
     try {
 
+        await models.Comment.create({
+            postIdx: idx,
+            userName: user.name,
+            comment: body.comment,
+        });
 
+        return res.status(200).json({
+            message: "댓글 작성 성공!",
+        });
  
     } catch(err) {
         console.log(err);
