c1f0280a7c3f251aa5c618f23e0053fff45b94da
박상아 <sangah0222@naver.com>
2020-11-07 23:47:38 +0900

    fdgs

--- GDproject_Web/src/pages/MainPage/MainPage.jsx
+++ GDproject_Web/src/pages/MainPage/MainPage.jsx
@@ -5,43 +5,30 @@ import MainLogin from '../../components/MainView/MainLoginModel/MainLoginModel'
 
 import logoPNG from './logo.png';
 import img1 from './Img/background.jpg';
+import arrowleft from './Img/arrowLeft.svg';
 import './MainPage.scss';
 
 const MainPage = () => {
 
   return (
-    <div className='Main'>
+    <div className="main">
 
-			<div className='logo_div'>
-   	   <img src={logoPNG} className='logo' alt='logo'/>
-  	  </div>
-      <div className='content'>
-
-				<div className='top'>
-						<img src={img1} className='img_file' alt='mainPic' />
-					</div>
-
-				<div className='button'>
-					<div className='button_box'>
-						<div className='start'></div>
-					</div>
-				</div>
-
-				<MainLogin className='LoginBox'/>
-
-				<div className='boxes'>
-					<div className='box1'>
-						공지사항
-					</div>
-					<div className='box2'>
-						인게임 사진
-					</div>
+		<div className="mainTop">
+			<div className="mainBackground">
+				<div className="mainContent">
+					<img className='logo' src={logoPNG} alt="logo" />
 				</div>
 			</div>
 
-    	<ViewBottom />
+			<div className="downLoad">
+				
+			</div>
 
-    	</div>
+			<div className="menu">
+
+			</div>
+		</div>
+	</div>
     )
 }
 
