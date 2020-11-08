a6794b96eea7cf5a7afeaacd6b4311ec8be6fa6f
박상아 <sangah0222@naver.com>
2020-11-08 12:29:59 +0900

    kj

--- GDproject_Web/src/pages/MainPage/MainPage.jsx
+++ GDproject_Web/src/pages/MainPage/MainPage.jsx
@@ -6,6 +6,7 @@ import MainLogin from '../../components/MainView/MainLoginModel/MainLoginModel'
 import logoPNG from './logo.png';
 import img1 from './Img/background.jpg';
 import arrowleft from './Img/arrowLeft.svg';
+import pumkin from './Img/pinkPumkinWeb.png';
 import './MainPage.scss';
 
 const MainPage = () => {
@@ -14,36 +15,48 @@ const MainPage = () => {
     <div className="all">
 
 		<div className="mainTop">
-			<div className="main">
-				<div className="mainBackground">
-					<div className="mainContent">
-						<img className='logo' src={logoPNG} alt="logo" />
-					</div>
+			<div className="mainBackground">
+				<div className="mainContent">
+					<img className='logo' src={logoPNG} alt="logo" />
+				</div>
+			</div>
+
+			<div className="menu">
+				<div className="downLoad">
+					<div className="icon"></div>
 				</div>
 
-				<div className="menu">
-					<div className="downLoad">
-						<div className="icon"></div>
-					</div>
+				<div className="menuTop">
+					<div className="gameStart"></div>
+				</div>
 
-					<div className="menuTop">
-						<div className="gameStart"></div>
-					</div>
-
-					<div className="menuBottom">
-						<div className="menuContent">
-
-						</div>
-					</div>
-
-					<div className="menuLogin">
+				<div className="menuBottom">
+					<div className="menuContent">
 
 					</div>
 				</div>
 
-				<div className="content">
-					
+				
+				<div className="menuLogin">
+					<MainLogin />
 				</div>
+			</div>
+
+			<div className="content">
+				<div className="noticeDec">
+					<img className="dec" src={pumkin} alt="pumkin"/>
+				</div>
+
+				<div className="mainNotice">
+
+				</div>
+
+				<div className="mainPost">
+
+				</div>
+			</div>
+			<div>
+				<ViewBottom />
 			</div>
 		</div>
 	</div>
