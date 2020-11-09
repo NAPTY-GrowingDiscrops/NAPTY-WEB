c7ec00e53ffff5f6e8565cd960f6904c72cfe04d
박상아 <sangah0222@naver.com>
2020-11-09 20:07:34 +0900

    w

--- GDproject_Web/src/components/Navigator/Navigator.jsx
+++ GDproject_Web/src/components/Navigator/Navigator.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
 import './Navigator.scss';
+import logo from '../../pages/MainPage/logo.png';
 
 const ViewTop = () => {
     return (
 			<div>
-				
+				<div className="topBox">
+                    <div className="top">
+                        <div className="logoBox">
+                            <img className="logo" src={logo} alt=""/>
+                        </div>
+                    </div>
+                </div>
 			</div>
     )
 }
