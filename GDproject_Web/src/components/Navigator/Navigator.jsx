fe425f3d92f03d952e2ec5ff773f4a6981a5e4af
박상아 <sangah0222@naver.com>
2020-11-09 23:02:09 +0900

    2

--- GDproject_Web/src/components/Navigator/Navigator.jsx
+++ GDproject_Web/src/components/Navigator/Navigator.jsx
@@ -8,7 +8,25 @@ const ViewTop = () => {
 			<div>
 				<div className="topBox">
                     <div className="top">
-                        
+                        <div className="logoBox">
+                            <img className="logo" src={logo} alt=""/>
+                        </div>
+
+                        <div className="menuBox">
+                            <div className="gameStart">
+
+                            </div>
+                            
+                            <div className="menuTop">
+                                <div className="menuContent">
+                                    
+                                </div>
+                            </div>
+
+                            <div className="menuBottom">
+
+                            </div>
+                        </div>
                     </div>
                 </div>
 			</div>
