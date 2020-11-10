929d43aae3e1ce8cd5ceedc4de147d00e5f90a2e
jungbin0722 <jungbin4337@dgsw.hs.kr>
2020-11-11 08:19:02 +0900

    [ADD] Login Form

--- GDproject_Web/src/components/AuthView/LoginModel/LoginModel.jsx
+++ GDproject_Web/src/components/AuthView/LoginModel/LoginModel.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import logo from '../logo_white.png'
 
+import './LoginModel.scss'
+
 const LoginModel = ({ setIsRegister }) => {
-    return (
-      <div className='LoginPage'>
-				<div className='logo'>
-					<img className='logo_img' src={logo} alt='logo_white' /> 
-				</div>
-				
+
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState(''); 
+
+  return (
+    <div className='LoginPage'>
+      <div className='logo'>
+        <img className='logo_img' src={logo} alt='logo_white' /> 
       </div>
-    )
+
+      <div className='input_boxes'>
+       
+        <p className='input_boxes_text'>이름</p>
+        <input className='input_boxes_input' type='text' value={id} onChange={e => setId(e.target.value)} />
+
+        <p className='input_boxes_text' style={{ marginTop: '16px' }}>비밀번호</p>
+        <input className='input_boxes_input' type='text' value={id} onChange={e => setId(e.target.value)} />
+
+      </div>
+
+      <div className='login_button_div'>
+        
+      </div>
+
+    </div>
+  )
 }
 
 export default LoginModel;
