6dedc1f5c1d7ba5d95099b5d4cd924084f7e8ec3
박상아 <sangah0222@naver.com>
2020-11-11 12:35:54 +0900

    f

--- GDproject_Web/src/components/InfoView/InfoView.jsx
+++ GDproject_Web/src/components/InfoView/InfoView.jsx
@@ -6,11 +6,25 @@ const InfoView = () => {
         <div className="all">
             <div className="mainBox">
                 <div className="mainLeftBox">
-
+                    <div className="leftTitle">
+                        <div className="leftContent textFocus">게임 세계관</div>
+                        <div className="leftContent textUnfocus">게임 조작법</div>
+                        <div className="leftContent textUnfocus">병작물 도감</div>
+                        <div className="leftContent textUnfocus">개발자 소개</div>
+                    </div>
                 </div>
 
                 <div className="mainRightBox">
-                    
+                    <div className="rightTopBox">
+                        <div className="rightTopMenu focus">게임 세계관</div>
+                        <div className="rightTopMenu unfocus">게임 조작법</div>
+                        <div className="rightTopMenu unfocus">병작물 도감</div>
+                        <div className="rightTopMenu unfocus">개발자 소개</div>
+                    </div>
+
+                    <div className="rightContentBox">
+                        
+                    </div>
                 </div>
             </div>
         </div>
