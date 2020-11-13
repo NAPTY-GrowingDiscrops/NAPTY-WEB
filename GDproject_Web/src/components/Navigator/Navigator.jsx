32d0fc39a7ab28871b0add9bcfd5325ca020b9e9
jungbin0722 <jungbin4337@dgsw.hs.kr>
2020-11-13 11:57:07 +0900

    [FIX] profile

--- GDproject_Web/src/components/Navigator/Navigator.jsx
+++ GDproject_Web/src/components/Navigator/Navigator.jsx
@@ -29,41 +29,41 @@ const ViewTop = () => {
 		}
 	}, []);
 
-		return (
-			<Router>
-				<div className="all">
-					<div className="topBox">
-						<div className="top">
-							<div className="logoBox">
-								<Link to='' onClick={() => {window.location.href='/'}}><img className='logo' src={logoPNG} alt="Logo"  /></Link>
-							</div>
+  return (
+    <Router>
+      <div className="all">
+        <div className="topBox">
+          <div className="top">
+            <div className="logoBox">
+              <Link to='' onClick={() => {window.location.href='/'}}><img className='logo' src={logoPNG} alt="Logo"  /></Link>
+            </div>
 
-							<div className="menu">
-								<div className="menuTop">
-									<div className="menuContent">
-										<Link to='' className='menuContents' onClick={() => {alert('아직 구현이 안되어 있어요!')}}>게임정보</Link>
-										<Link to='' className='menuContents' onClick={() => {alert('아직 구현이 안되어 있어요!')}}>게시판</Link>
-										<Link to='' className='menuContents' style={{ marginRight: '0px'}} onClick={() => {alert('아직 구현이 안되어 있어요!')}}>고객센터</Link>
-										{ isLogin ?
-											<div>
-												<button className="logout" onClick={() => {logout()}}>로그아웃</button>
-												<div className="info">
-													<p className="name">{name}<span className="sla">님</span></p>
-													<div className="profile"></div>
-												</div>
-											</div>
-											: <button className="logout" onClick={() => {window.location.href=`/auth/${uuid.v4()}`}}>로그인</button>
-										}
-									</div>
-								</div>
+            <div className="menu">
+              <div className="menuTop">
+                <div className="menuContent">
+                  <Link to='' className='menuContents' onClick={() => {alert('아직 구현이 안되어 있어요!')}}>게임정보</Link>
+                  <Link to='' className='menuContents' onClick={() => {alert('아직 구현이 안되어 있어요!')}}>게시판</Link>
+                  <Link to='' className='menuContents' style={{ marginRight: '0px'}} onClick={() => {alert('아직 구현이 안되어 있어요!')}}>고객센터</Link>
+                  { isLogin ?
+                    <div>
+                      <button className="logout" onClick={() => {logout()}}>로그아웃</button>
+                      <div className="info">
+                        <p className="name">{name}<span className="sla">님</span></p>
+                        <div className="profile"></div>
+                      </div>
+                    </div>
+                    : <button className="logout" onClick={() => {window.location.href=`/auth/${uuid.v4()}`}}>로그인</button>
+                  }
+                </div>
+              </div>
 
-								<div className="menuBottom">
-									<p className="gameStart">게임시작</p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
+              <div className="menuBottom">
+                <p className="gameStart">게임시작</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 		</Router>
 	)
 }
